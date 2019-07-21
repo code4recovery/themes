2c2fe89d79121f2bc1ebf8f1b69fdb6872a008b6
Josh Reisner <josh@joshreisner.com>
2019-07-21 11:49:43 -0500

    changing headline font to match new site design

--- aaws/includes/assets.php
+++ aaws/includes/assets.php
@@ -2,7 +2,7 @@
 
 //gutenberg customizations
 add_action('enqueue_block_editor_assets', function() {
-	wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Roboto|Roboto+Slab:700');
+	wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Roboto|Merriweather:900');
 	wp_enqueue_style('mytheme-block-editor-styles', get_theme_file_uri('/assets/editor.css'), false, '1.0', 'all');
 });
 
@@ -13,7 +13,7 @@ add_action('admin_enqueue_scripts', function() {
 
 //include js / css on public pages
 add_action('wp_enqueue_scripts', function() {
-	wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Roboto|Roboto+Slab:700');
+	wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Roboto|Merriweather:900');
 	wp_enqueue_style('aaws', get_stylesheet_directory_uri() . '/assets/compiled/theme.css', array(), filemtime(get_stylesheet_directory() . '/assets/compiled/theme.css'));
 	//wp_enqueue_script('aaws', get_stylesheet_directory_uri() . '/assets/compiled/theme.js', array(), filemtime(get_stylesheet_directory() . '/assets/compiled/theme.js'), true);
 });
