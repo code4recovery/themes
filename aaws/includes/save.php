166e9817fac40450a32742437efba6391e24c505
Josh Reisner <josh@joshreisner.com>
2019-07-30 07:18:32 -0400

    updating email address

--- aaws/includes/save.php
+++ aaws/includes/save.php
@@ -65,7 +65,7 @@ function aaws_contact() {
 	}
 
 	//send email message
-	wp_mail('meetingguide@aa.org', '[' . get_bloginfo('name') . '] ' . $_POST['organization'], '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+	wp_mail('appsupport@aa.org', '[' . get_bloginfo('name') . '] ' . $_POST['organization'], '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 	<html xmlns="http://www.w3.org/1999/xhtml">
 		<head>
 			<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
